992ec98b4e1b59ad0fc82a8a1538b51be52c56d7
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-11 18:49:05 +0530

    adding animation css

--- navigation.js
+++ navigation.js
@@ -5,24 +5,42 @@ const nav2 = document.getElementById("nav-2");
 const nav3 = document.getElementById("nav-3");
 const nav4 = document.getElementById("nav-4");
 const nav5 = document.getElementById("nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+function navAnimation(direction1, direction2) {
+  navItems.forEach((item, idx) => {
+    item.classList.replace(
+      `slide-${direction1}-${idx + 1}`,
+      `slide-${direction2}-${idx + 1}`
+    );
+  });
+}
+// function navAnimation(direction1, direction2) {
+//   navItems.forEach((item, index) => {
+//     item.classList.replace(
+//       `slide-${direction1}-${index + 1}`,
+//       `slide-${direction2}-${index + 1}`
+//     );
+//   });
+// }
 
 function toggleNav() {
   menuBars.classList.toggle("change");
   overlay.classList.toggle("overlay-active");
   if (overlay.classList.contains("overlay-active")) {
     // Animate In - Overlay
-    overlay.classList.add("overlay-slide-right");
-    overlay.classList.remove("overlay-slide-left");
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+    // Animated In - Nav Items
+    navAnimation("out", "in");
   } else {
     // Animate Out - Overlay
-    overlay.classList.add("overlay-slide-left");
-    overlay.classList.remove("overlay-slide-right");
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+    // Animated Out - Nav Items
+    navAnimation("in", "out");
   }
 }
 
 menuBars.addEventListener("click", toggleNav);
-nav1.addEventListener("click", toggleNav);
-nav2.addEventListener("click", toggleNav);
-nav3.addEventListener("click", toggleNav);
-nav4.addEventListener("click", toggleNav);
-nav5.addEventListener("click", toggleNav);
+navItems.forEach((nav) => {
+  nav.addEventListener("click", toggleNav);
+});

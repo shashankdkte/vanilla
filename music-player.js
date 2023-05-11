91bcea9233dbbd27f30db69baa68241cb81c2e19
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-11 21:01:28 +0530

    Added Next Previous functionality

--- music-player.js
+++ music-player.js
@@ -1,10 +1,38 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
 const audio = document.querySelector("audio");
 const playBtn = document.getElementById("play");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 
 let isPlaying = false;
-
+const songs = [
+  {
+    nameSong: "jacinto-1",
+    imageName: "img-1",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    nameSong: "jacinto-2",
+    imageName: "img-2",
+    displayName: "Seven Nation Army (Remix)",
+    artist: "Jacinto Design",
+  },
+  {
+    nameSong: "jacinto-3",
+    imageName: "img-3",
+    displayName: "Goodnight, Disco Queen",
+    artist: "Jacinto Design",
+  },
+  {
+    nameSong: "metric-1",
+    imageName: "metric-1",
+    displayName: "Front Row (Remix)",
+    artist: "Metric/Jacinto Design",
+  },
+];
 function playSong() {
   isPlaying = true;
   playBtn.classList.replace("fa-play", "fa-pause");
@@ -22,3 +50,36 @@ function pauseSong() {
 playBtn.addEventListener("click", () => {
   isPlaying ? pauseSong() : playSong();
 });
+
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  audio.src = `music/${song.nameSong}.mp3`;
+  image.src = `images/${song.imageName}.jpg`;
+}
+
+let songIndex = 0;
+
+// Previous Song
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+loadSong(songs[songIndex]);
+
+// Event Listeners
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
